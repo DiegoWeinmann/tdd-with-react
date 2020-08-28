611f20a51a6642d1bede84707bbf1c311b153f21
Diego Weinmann <diego.weinmann@hotmail.com>
2020-08-28 20:54:59 -0300

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function App() {
-	const [count, setCount] = useState(0);
-	const [error, setError] = useState(false);
-
-	const increment = () => {
-		if (error && count === 0) {
-			setError(false);
-			setCount(count + 1);
-		} else {
-			setCount(count + 1);
-		}
-	};
-
-	const decrement = () => {
-		if (count > 0) {
-			return setCount(count - 1);
-		} else {
-			setError(true);
-			setTimeout(() => setError(false), 3000);
-		}
-	};
-
+const App = () => {
 	return (
-		<div role="main">
-			<h1>Counter App</h1>
-			<button onClick={increment}>increment counter</button>
-			<button onClick={decrement}>decrement counter</button>
-			<div aria-label="counter display">Count: {count}</div>
-			{error && <div>Error: You can't go below of zero.</div>}
+		<div>
+			<h1>React App</h1>
 		</div>
 	);
-}
+};
 
 export default App;
