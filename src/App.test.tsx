81e6119c13466dcf847cd67d507b2ce9a9c76cf9
Diego Weinmann <diego.weinmann@hotmail.com>
2020-08-28 20:48:12 -0300

    test: error message and decrement button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,51 +4,64 @@ import userEvent from '@testing-library/user-event';
 import App from './App';
 
 beforeEach(() => {
-  render(<App />);
-  configure({
-    throwSuggestions: true,
-  });
+	render(<App />);
+	configure({
+		throwSuggestions: true,
+	});
 });
 
 describe('App', () => {
-  it('should render without an error', () => {
-    expect(screen.getByRole('main')).toBeInTheDocument();
-  });
+	it('should render without an error', () => {
+		expect(screen.getByRole('main')).toBeInTheDocument();
+	});
 
-  it('should have Counter App as heading', () => {
-    expect(screen.getByRole('heading').innerHTML).toBe('Counter App');
-  });
+	it('should have Counter App as heading', () => {
+		expect(screen.getByRole('heading').innerHTML).toBe('Counter App');
+	});
 
-  it('should render a counter display', () => {
-    expect(screen.getByLabelText('counter display')).toBeTruthy();
-  });
+	it('should render a counter display', () => {
+		expect(screen.getByLabelText('counter display')).toBeInTheDocument();
+	});
 
-  it('should render an increment button', () => {
-    expect(
-      screen.getByRole('button', { name: 'increment counter' })
-    ).toBeTruthy();
-  });
+	it('should render an increment button', () => {
+		expect(
+			screen.getByRole('button', { name: 'increment counter' })
+		).toBeInTheDocument();
+	});
 
-  it('should render a decrement button', () => {
-    expect(
-      screen.getByRole('button', { name: 'decrement counter' })
-    ).toBeTruthy();
-  });
+	it('should render a decrement button', () => {
+		expect(
+			screen.getByRole('button', { name: 'decrement counter' })
+		).toBeInTheDocument();
+	});
 
-  it('should start the counter at 0', () => {
-    expect(screen.getByLabelText('counter display')).toHaveTextContent(/0/);
-  });
+	it('should start the counter at 0', () => {
+		expect(screen.getByLabelText('counter display')).toHaveTextContent(/0/);
+	});
 
-  it('should increment the counter when increment button is clicked', () => {
-    const incButton = screen.getByRole('button', { name: /increment/i });
-    userEvent.click(incButton);
-    expect(screen.getByLabelText('counter display')).toHaveTextContent(/1/);
-  });
+	it('should increment the counter when increment button is clicked', () => {
+		const incButton = screen.getByRole('button', { name: /increment/i });
+		userEvent.click(incButton);
+		expect(screen.getByLabelText('counter display')).toHaveTextContent(/1/);
+	});
 
-  it('should decrement the counter when decrement button is clicked', async () => {
-    const decButton = screen.getByRole('button', { name: /decrement/i });
-    const counter = screen.getByLabelText('counter display');
-    userEvent.click(decButton);
-    expect(counter).toHaveTextContent(/-1/);
-  });
+	it('should decrement the counter when decrement button is clicked', async () => {
+		const incButton = screen.getByRole('button', { name: /increment/i });
+		const decButton = screen.getByRole('button', { name: /decrement/i });
+		const counter = screen.getByLabelText('counter display');
+		userEvent.click(incButton);
+		userEvent.click(decButton);
+		expect(counter).toHaveTextContent(/0/);
+	});
+
+	it('should show an error message if the user tries to decrement when the counter has a value of 0', () => {
+		expect(screen.queryByText(/Error/i)).not.toBeInTheDocument();
+		const decButton = screen.getByRole('button', { name: /decrement/i });
+		const incButton = screen.getByRole('button', { name: /increment/i });
+		userEvent.click(decButton);
+		expect(screen.queryByLabelText('counter display')).toHaveTextContent('0');
+		expect(screen.queryByText(/Error/i)).toBeInTheDocument();
+		userEvent.click(incButton);
+		expect(screen.queryByText(/Error/i)).not.toBeInTheDocument();
+	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
+	const [error, setError] = useState(false);
 
-  return (
-    <div role='main'>
-      <h1>Counter App</h1>
-      <button onClick={() => setCount(count + 1)}>increment counter</button>
-      <button onClick={() => setCount(count - 1)}>decrement counter</button>
-      <div aria-label='counter display'>Count: {count}</div>
-    </div>
-  );
+	const increment = () => {
+		if (error && count === 0) {
+			setError(false);
+			setCount(count + 1);
+		} else {
+			setCount(count + 1);
+		}
+	};
+
+	const decrement = () => {
+		if (count > 0) {
+			return setCount(count - 1);
+		} else {
+			setError(true);
+			setTimeout(() => setError(false), 3000);
+		}
+	};
+
+	return (
+		<div role="main">
+			<h1>Counter App</h1>
+			<button onClick={increment}>increment counter</button>
+			<button onClick={decrement}>decrement counter</button>
+			<div aria-label="counter display">Count: {count}</div>
+			{error && <div>Error: You can't go below of zero.</div>}
+		</div>
+	);
 }
 
 export default App;
